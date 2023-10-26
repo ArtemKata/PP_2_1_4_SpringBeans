9dbc83de346cc1f025643e6084eb603225f4fa42
Artyom Kazragis <kazragis.artem2003@mail.ru>
2023-10-26 22:09:10 +0700

    2.1.4

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,12 +1,23 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
+    @Autowired
+    private Timer timer;
+
+    public AnimalsCage(Timer timer) {
+        this.timer = timer;
+    }
+    public Timer getTimer(){
+        return timer;
+    }
 
     @Autowired
+    @Qualifier("Dog")
     private Animal animal;
 
     public void whatAnimalSay() {
